ce0870ad26d343872916850b83453c541586c061
Xander089 <alessandro.lombardi@gmail.com>
2023-04-23 12:04:48 +0200

    removed initial reset

--- src/data/write.ts
+++ src/data/write.ts
@@ -1,314 +1,135 @@
 import { Game } from "../types/Game";
 import { MonsterSelection, Player } from "../types/GameTypes";
 
-const players: Player[] = [
-    {id: 0,
-    name: "Ale",
-    category: "",
-    type: "player",
-    hp: 5,
-    currentHp: 5,
-    initiative: 2,
-    strength: 1,
-    dexterity: 2,
-    constitution: 3,
-    intelligence: 3,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: true,
-    status: "",
-    statusDuration: 0,
-    sortIndex: -1,
-  },
-  {
-    id: 1,
-    name: "Ste",
-    category: "",
-    type: "player",
-    hp: 5,
-    currentHp: 5,
-    initiative: 3,
-    strength: 1,
-    dexterity: 2,
-    constitution: 3,
-    intelligence: 3,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: true,
-    status: "",
-    statusDuration: 0,
-    sortIndex: -1,
-  },
-  {
-    id: 2,
-    name: "Fab",
-    category: "",
-    type: "player",
-    hp: 5,
-    currentHp: 5,
-    initiative: 4,
-    strength: 1,
-    dexterity: 2,
-    constitution: 3,
-    intelligence: 3,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: true,
-    status: "PSN",
-    statusDuration: 0,
-
-    sortIndex: -1,
-  },
-
-  {
-    id: 3,
-    name: "",
-    category: "Goblin",
-    type: "monster",
-    hp: 5,
-    currentHp: 5,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 3,
-    intelligence: 3,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: false,
-    status: "",
-    statusDuration: 0,
-
-    sortIndex: -1,
-  },
-  {
-    id: 4,
-    name: "",
-    category: "Troll",
-    type: "monster",
-    hp: 6,
-    currentHp: 6,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 4,
-    intelligence: 1,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: false,
-    status: "",
-    statusDuration: 0,
-
-    sortIndex: -1,
-  },
-  {
-    id: 5,
-    name: "",
-    category: "Cerberus",
-    type: "monster",
-    hp: 6,
-    currentHp: 6,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 4,
-    intelligence: 1,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: false,
-    status: "",
-    statusDuration: 0,
-    sortIndex: -1,
-  },
-
-
-  {
-    id: 6,
-    name: "Goblin1",
-    category: "Goblin",
-    type: "monster",
-    hp: 5,
-    currentHp: 5,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 3,
-    intelligence: 3,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: true,
-    status: "",
-    statusDuration: 0,
-
-    sortIndex: -1,
-  },
-  {
-    id: 7,
-    name: "Troll1",
-    category: "Troll",
-    type: "monster",
-    hp: 6,
-    currentHp: 6,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 4,
-    intelligence: 1,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: true,
-    status: "",
-    statusDuration: 0,
-
-    sortIndex: -1,
-  },
-  {
-    id: 8,
-    name: "Cerberus1",
-    category: "Cerberus",
-    type: "monster",
-    hp: 6,
-    currentHp: 6,
-    initiative: 1,
-    strength: 1,
-    dexterity: 2,
-    constitution: 4,
-    intelligence: 1,
-    wisdom: 3,
-    charisma: 2,
-    isPlaying: false,
-    status: "",
-    statusDuration: 0,
-    sortIndex: -1,
-  },
-];
-
 export class Dao {
-
-  static updateStatusDuration(step:number){
-    const playingAndStatusActive = [...this.getPlayers('player'),...this.getPlayers('monster')]
-                    ?.filter(p => p.isPlaying && p.statusDuration > 0)
+  static updateStatusDuration(step: number) {
+    const playingAndStatusActive = [
+      ...this.getPlayers("player"),
+      ...this.getPlayers("monster"),
+    ]?.filter((p) => p.isPlaying && p.statusDuration > 0);
     //se premo avanti step = 1 e decremento la duration
-    //se premo indietro step = -1 e incremento la duration                
-    playingAndStatusActive?.forEach(p =>{
-      p.statusDuration-=step
-      if(p.statusDuration === 0){
-        p.status = ''
+    //se premo indietro step = -1 e incremento la duration
+    playingAndStatusActive?.forEach((p) => {
+      p.statusDuration -= step;
+      if (p.statusDuration === 0) {
+        p.status = "";
       }
-    })
-    playingAndStatusActive?.forEach(p => this.writePlayer(p))
+    });
+    playingAndStatusActive?.forEach((p) => this.writePlayer(p));
   }
 
-  static addMonstersToGame(categories: MonsterSelection[]){
+  static addMonstersToGame(categories: MonsterSelection[]) {
     categories
-    ?.filter(cat => cat.isSelected && cat.quantity > 0)
-    ?.forEach(cat => this.addMonsterToGame(cat))
+      ?.filter((cat) => cat.isSelected && cat.quantity > 0)
+      ?.forEach((cat) => this.addMonsterToGame(cat));
   }
 
-  static addMonsterToGame(category : MonsterSelection){     
-    const selectedCategory = this.getPlayers("monster")
-        ?.filter((m) => m?.name === "" && m.category === category.category)[0]
-      const count = this.getPlayingCategoryCount(category)
-      let i = 0
-      while(i < category.quantity){
-        const stamp  : Omit<Player, 'id'>=   {
-          name: selectedCategory.category + (i+1 + count),
-          category: selectedCategory.category,
-          type: "monster",
-          hp: selectedCategory.hp,
-          currentHp: selectedCategory.currentHp,
-          initiative: Math.ceil(Math.random() * 20), //roll initiative
-          strength: selectedCategory.strength,
-          dexterity: selectedCategory.dexterity,
-          constitution: selectedCategory.constitution,
-          intelligence: selectedCategory.initiative,
-          wisdom: selectedCategory.wisdom,
-          charisma: selectedCategory.charisma,
-          isPlaying: true,
-          status: "",
-          statusDuration: 0,
-        }  
-        this.addPlayer(stamp)
-        i+=1
-      }
+  static addMonsterToGame(category: MonsterSelection) {
+    const selectedCategory = this.getPlayers("monster")?.filter(
+      (m) => m?.name === "" && m.category === category.category
+    )[0];
+    const count = this.getPlayingCategoryCount(category);
+    let i = 0;
+    while (i < category.quantity) {
+      const stamp: Omit<Player, "id"> = {
+        name: selectedCategory.category + (i + 1 + count),
+        category: selectedCategory.category,
+        type: "monster",
+        hp: selectedCategory.hp,
+        currentHp: selectedCategory.currentHp,
+        initiative: Math.ceil(Math.random() * 20), //roll initiative
+        strength: selectedCategory.strength,
+        dexterity: selectedCategory.dexterity,
+        constitution: selectedCategory.constitution,
+        intelligence: selectedCategory.initiative,
+        wisdom: selectedCategory.wisdom,
+        charisma: selectedCategory.charisma,
+        isPlaying: true,
+        status: "",
+        statusDuration: 0,
+      };
+      this.addPlayer(stamp);
+      i += 1;
+    }
   }
 
-  static getPlayingCategoryCount(category: MonsterSelection): number{
-    return this.getPlayers("monster")
-    ?.filter((m) => m?.name !== "" && m.category === category.category && m.isPlaying)
-    ?.length ?? 0
+  static getPlayingCategoryCount(category: MonsterSelection): number {
+    return (
+      this.getPlayers("monster")?.filter(
+        (m) => m?.name !== "" && m.category === category.category && m.isPlaying
+      )?.length ?? 0
+    );
   }
-  
+
   static removePlayer(id: number, type: string) {
-      if(type === "monster"){
-          this.deletePlayer(id)
-      }
-      else {
-         const player = this.findPlayer(id)
-         const newPlayer : Partial<Player> = {
-          id: player?.id,
-          name: player?.name,
-          category: player?.category,
-          type: player?.type,
-          hp: player?.hp,
-          currentHp: player?.currentHp,
-          initiative: player?.initiative,
-          strength: player?.strength,
-          dexterity: player?.dexterity,
-          constitution: player?.constitution,
-          intelligence: player?.intelligence,
-          wisdom: player?.wisdom,
-          charisma: player?.charisma,
-          isPlaying: false,
-          status: player?.status,
-          statusDuration: player?.statusDuration,
-         }
-         this.writePlayer(newPlayer)
-      }
-  }
-
-  static getAllLocalStorage(): string{
-    return this.getAllKeys()?.map(it => it + '######' + (localStorage.getItem(it)?? '')).join('|||') ?? ''
-  }
-  static writeAllLocalStorage(text: string){
-    if(text.length == 0){
-      localStorage.clear()
-      return
+    if (type === "monster") {
+      this.deletePlayer(id);
+    } else {
+      const player = this.findPlayer(id);
+      const newPlayer: Partial<Player> = {
+        id: player?.id,
+        name: player?.name,
+        category: player?.category,
+        type: player?.type,
+        hp: player?.hp,
+        currentHp: player?.currentHp,
+        initiative: player?.initiative,
+        strength: player?.strength,
+        dexterity: player?.dexterity,
+        constitution: player?.constitution,
+        intelligence: player?.intelligence,
+        wisdom: player?.wisdom,
+        charisma: player?.charisma,
+        isPlaying: false,
+        status: player?.status,
+        statusDuration: player?.statusDuration,
+      };
+      this.writePlayer(newPlayer);
     }
-      localStorage.clear()
-      text.split('|||')
-      ?.forEach(kv => {
-        console.log("keyValue",kv)
-        const keyValue = kv?.split('######')
-        localStorage.setItem(keyValue[0],keyValue[1])
-        
-      })
-
   }
 
-  static writeAll() {
-    players?.forEach((p) => this.writePlayer(p));
+  static getAllLocalStorage(): string {
+    return (
+      this.getAllKeys()
+        ?.map((it) => it + "######" + (localStorage.getItem(it) ?? ""))
+        .join("|||") ?? ""
+    );
   }
-
-  static  addPlayer(player : Omit<Player,'id'> | Partial<Omit<Player,'id'>>): Player{
-      const newId = this.getMaxKey() + 1
-      const result = {id: newId,...player} as Player
-      this.writePlayer(result)
-      return result
-  }
-
-  static getMaxKey(): number{
-    const players = this.getPlayers('player')
-    const monsters = this.getPlayers('monster')
-    const all = [...players,...monsters]
-    if(all.length > 0){
-      const ids = all?.map(player => player.id)
-    return Math.max(...ids) ?? 0
+  static writeAllLocalStorage(text: string) {
+    if (text.length == 0) {
+      localStorage.clear();
+      return;
     }
-    return 0
+    localStorage.clear();
+    text.split("|||")?.forEach((kv) => {
+      console.log("keyValue", kv);
+      const keyValue = kv?.split("######");
+      localStorage.setItem(keyValue[0], keyValue[1]);
+    });
+  }
+
+  static addPlayer(
+    player: Omit<Player, "id"> | Partial<Omit<Player, "id">>
+  ): Player {
+    const newId = this.getMaxKey() + 1;
+    const result = { id: newId, ...player } as Player;
+    this.writePlayer(result);
+    return result;
+  }
+
+  static getMaxKey(): number {
+    const players = this.getPlayers("player");
+    const monsters = this.getPlayers("monster");
+    const all = [...players, ...monsters];
+    if (all.length > 0) {
+      const ids = all?.map((player) => player.id);
+      return Math.max(...ids) ?? 0;
+    }
+    return 0;
   }
 
   static writePlayer(player: Player | Partial<Player>) {
-    const key = player?.id + '_'  + player?.type;
+    const key = player?.id + "_" + player?.type;
     if (key !== undefined) {
       localStorage.setItem(key, JSON.stringify(player));
     }
@@ -318,11 +139,10 @@ export class Dao {
     const result = [];
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i) ?? "";
-      result.push(key)
+      result.push(key);
     }
     return result;
   }
-
 
   static getKeys(query: string): string[] {
     const result = [];
@@ -344,9 +164,7 @@ export class Dao {
           if (player?.id === id) {
             return player as Player;
           }
-        } catch (e: any) {
-          // return undefined;
-        }
+        } catch (e: any) {}
       }
     }
   }
@@ -368,12 +186,14 @@ export class Dao {
     this.getPlayers(type)?.forEach((p) => this.deletePlayer(p?.id));
   }
 
-  static getPlayingMembersCount(): number{
-    const party = this.getPlayers("player")?.filter((m) => m.isPlaying === true);
+  static getPlayingMembersCount(): number {
+    const party = this.getPlayers("player")?.filter(
+      (m) => m.isPlaying === true
+    );
     const monsters = this.getPlayers("monster")?.filter(
       (m) => m.isPlaying === true && m.name !== ""
     );
-    return [...party,...monsters].length ?? 0
+    return [...party, ...monsters].length ?? 0;
   }
 
   static getEmptyGame(): Game {
@@ -383,7 +203,7 @@ export class Dao {
       count: this.getPlayingMembersCount(),
       effectName: "",
       effectDuration: 0,
-    }
+    };
   }
 
   static getGame(): Game {
@@ -391,7 +211,7 @@ export class Dao {
     if (game !== "") {
       return JSON.parse(game) as Game;
     } else {
-      return this. getEmptyGame();
+      return this.getEmptyGame();
     }
   }
 
@@ -399,44 +219,44 @@ export class Dao {
     localStorage.setItem("TheGame", JSON.stringify(g));
   }
 
-  static refreshGameTurnStepsCount(){
-        const current = this.getGame()
-        const newGame: Game = {
-          turn: current.turn,
-          step: current.step,
-          count: this.getPlayingMembersCount(),
-          effectName: current.effectName,
-          effectDuration: current.effectDuration,
-        }
-        this.writeGame(newGame)
+  static refreshGameTurnStepsCount() {
+    const current = this.getGame();
+    const newGame: Game = {
+      turn: current.turn,
+      step: current.step,
+      count: this.getPlayingMembersCount(),
+      effectName: current.effectName,
+      effectDuration: current.effectDuration,
+    };
+    this.writeGame(newGame);
   }
 
-  static updateGameEffect(name:string,duration: number){
-    const current = this.getGame()
+  static updateGameEffect(name: string, duration: number) {
+    const current = this.getGame();
     const newGame: Game = {
       turn: current.turn,
       step: current.step,
       count: current.count,
       effectName: name,
       effectDuration: duration * current.count,
-    }
-    this.writeGame(newGame)
-}
+    };
+    this.writeGame(newGame);
+  }
 
   static resetGame() {
     localStorage.setItem("TheGame", JSON.stringify(this.getEmptyGame()));
   }
 }
 
-
-
 export class History {
   static #history = "History";
-  static getAll(end: number = 100): string[] { //faccio sì che la storia non restituisca + di 100 elementi
-    const history = JSON.parse(localStorage?.getItem(this.#history) ?? "[]") as string[]
-    return history.slice(0,end);
+  static getAll(end: number = 100): string[] {
+    //faccio sì che la storia non restituisca + di 100 elementi
+    const history = JSON.parse(
+      localStorage?.getItem(this.#history) ?? "[]"
+    ) as string[];
+    return history.slice(0, end);
   }
-  
 
   static deleteAll() {
     localStorage?.removeItem(this.#history);
@@ -444,7 +264,7 @@ export class History {
   static addLine(line: string) {
     localStorage?.setItem(
       this.#history,
-      JSON.stringify([line,...this.getAll(99)]) //faccio sì che la storia non contenga + di 100 elementi
+      JSON.stringify([line, ...this.getAll(99)]) //faccio sì che la storia non contenga + di 100 elementi
     );
   }
 }
