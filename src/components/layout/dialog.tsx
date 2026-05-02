4d1b6376bbaa465965d55d3179c014371b4de1e3
Xander089 <alessandro.lombardi@gmail.com>
2026-05-02 13:58:18 +0200

    modali fix

--- src/components/layout/dialog.tsx
+++ src/components/layout/dialog.tsx
@@ -1,12 +1,14 @@
+import { createPortal } from "react-dom";
 import styles from "./Dialog.module.css";
 
 export default function Dialog(props: any) {
   const width = props?.customModal ?? "30%";
-  return (
+  return createPortal(
     <div className={styles.modal}>
       <div style={{ width: width }} className={styles.modalContent}>
         {props.children}
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
